cb124094e07d4b97c8e62586a1223432164be74c
Ryoma <ryoma-android@users.noreply.github.com>
2025-06-27 23:13:16 +0900

    11

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         {/* iOS用のPWAフルスクリーン表示 */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="高橋ホーム" />
         {/* その他必要なタグ */}
